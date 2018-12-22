3ea50df3de046d48f278d3321e7220d559ea9f3d
abc <abc@example.com>
2018-12-22 15:47:11 -0200

    My Commit

---
+++ FORCA/ForcaJogo/Inicio.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ForcaJogo
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPalavra = new System.Windows.Forms.TextBox();
+            this.txtDica = new System.Windows.Forms.TextBox();
+            this.btnJogar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite a palavra aqui:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(624, 44);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Informe uma dica sobre a palavra:";
+            // 
+            // txtPalavra
+            // 
+            this.txtPalavra.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPalavra.Location = new System.Drawing.Point(20, 95);
+            this.txtPalavra.Name = "txtPalavra";
+            this.txtPalavra.Size = new System.Drawing.Size(616, 63);
+            this.txtPalavra.TabIndex = 2;
+            this.txtPalavra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPalavra_KeyPress);
+            // 
+            // txtDica
+            // 
+            this.txtDica.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDica.Location = new System.Drawing.Point(12, 266);
+            this.txtDica.Name = "txtDica";
+            this.txtDica.Size = new System.Drawing.Size(616, 63);
+            this.txtDica.TabIndex = 3;
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnJogar.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogar.Location = new System.Drawing.Point(267, 364);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(140, 56);
+            this.btnJogar.TabIndex = 4;
+            this.btnJogar.Text = "Jogar";
+            this.btnJogar.UseVisualStyleBackColor = false;
+            this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(647, 450);
+            this.Controls.Add(this.btnJogar);
+            this.Controls.Add(this.txtDica);
+            this.Controls.Add(this.txtPalavra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Inicio";
+            this.Text = "Inicio";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Inicio_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPalavra;
+        private System.Windows.Forms.TextBox txtDica;
+        private System.Windows.Forms.Button btnJogar;
+    }
+}
---
+++ FORCA/ForcaJogo/Inicio.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ForcaJogo
+{
+    public partial class Inicio : Form
+    {
+        public static string palavra, dica;
+
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            palavra = txtPalavra.Text;
+            dica = txtDica.Text;
+
+            if (txtPalavra.Text != "" && palavra[0] != ' ') {
+
+                Form1 f = new Form1();
+                f.Show();
+                this.Visible = this.Enabled = false;
+
+            } else {
+
+                MessageBox.Show("Informe uma palavra válida!");
+            
+            }
+        }
+
+        private void txtPalavra_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!char.IsLetter(e.KeyChar)) && (!char.IsControl(e.KeyChar)) 
+                && (!char.IsWhiteSpace(e.KeyChar)) && (e.KeyChar != '-'))
+                e.Handled = true;
+
+        }
+
+        private void Inicio_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
